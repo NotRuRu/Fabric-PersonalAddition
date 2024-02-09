537fc845981c4fe6f5a984c795a98294d53bb887
NotRuRu <rineherobb@gmail.com>
2024-02-09 22:04:10 +0100

    lowered the version of the fabric loader  and balanced saturation/hunger you get from food

--- src/main/java/net/notruru/personaladdition/item/ModFoodComponents.java
+++ src/main/java/net/notruru/personaladdition/item/ModFoodComponents.java
@@ -3,8 +3,8 @@ package net.notruru.personaladdition.item;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent Cooked_Egg = new FoodComponent.Builder().hunger(4).saturationModifier(0.16f).build();
+    public static final FoodComponent Cooked_Egg = new FoodComponent.Builder().hunger(4).saturationModifier(0.23f).build();
     public static final FoodComponent Honeyed_Apple = new FoodComponent.Builder().hunger(5).saturationModifier(0.4f).build();
-    public static final FoodComponent Toasted_Bread = new FoodComponent.Builder().hunger(5).saturationModifier(0.69f).build();
-    public static final FoodComponent Apple_pie = new FoodComponent.Builder().hunger(10).saturationModifier(0.5f).build();
+    public static final FoodComponent Toasted_Bread = new FoodComponent.Builder().hunger(6).saturationModifier(0.65f).build();
+    public static final FoodComponent Apple_pie = new FoodComponent.Builder().hunger(9).saturationModifier(0.4f).build();
 }
